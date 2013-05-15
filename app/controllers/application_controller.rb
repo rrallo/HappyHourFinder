5f474d911eed4f41737b579ab8f4cb8f633f1604
Lester Villaspir <lester.villaspir@gmail.com>
2013-05-15 12:47:27 -0700

    Removed ApplicationController check_url code. Doesn't apply to facebook login problem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :check_url
-
-  #redirecting the herokuapp and www version of domain
-  def check_url
-    url = request.url
-    if url.include?('appname.herokuapp.com')
-      redirect_to ('http://domain.com')
-    elsif url.include?('www.domain.com')
-      redirect_to ('http://domain.com')        
-    end    
-  end
 end
