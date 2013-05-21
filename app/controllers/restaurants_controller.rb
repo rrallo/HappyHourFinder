14c0a0ee41b53a5e4dc2277a26ca485a7568d33d
David Drabik <djdrabik@gmail.com>
2013-05-20 23:59:54 -0700

    new styles for index and restaurant pages

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,18 +3,27 @@ require 'oauth'
 
 class RestaurantsController < ApplicationController
 	def index
-    if params[:search]
-      @restaurant_results = Restaurant.search(params[:search])
-      params[:search] = nil
-    end
+	    if params[:search]
+	      @restaurant_results = Restaurant.search(params[:search])
+	      params[:search] = nil
+	    end
 
-    @restaurants = Restaurant.all
+	    @restaurants = Restaurant.all
+	    @list = {}
+	    @restaurants.each do |r|
+	    	yelp = connectYelp r
+	    	@list[r.name] = yelp["image_url"].gsub("ms\.jpg", "ls.jpg")
+	    end
 	end
 
 	def show
 		@restaurant = Restaurant.find(params[:id])
+		@yelp = connectYelp @restaurant
+		@header_img = @restaurant.name.downcase.tr(" ", "_") + ".jpg"
+  	end
 
-		consumer_key = 'ln-FI2hVPWjx4xFhWM5fGw'
+  	def connectYelp restaurant
+  		consumer_key = 'ln-FI2hVPWjx4xFhWM5fGw'
 		consumer_secret = 'QVauHsbQnYGdNoMMf5tRyw3doFk'
 		token = 'LAZBDQVnH-ZmlFa0U15jcWcTTbayNE_y'
 		token_secret = 'iPGhsMEKy-WmU-kK24bwxpryagM'
@@ -24,9 +33,9 @@ class RestaurantsController < ApplicationController
 		consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
 		access_token = OAuth::AccessToken.new(consumer, token, token_secret)
 
-		path = "/v2/business/" + @restaurant.yelp_id
+		path = "/v2/business/" + restaurant.yelp_id
 
 		p = access_token.get(path).body
-		@yelp = ActiveSupport::JSON.decode(p)
-  end
+		ActiveSupport::JSON.decode(p)
+	end
 end
