24adab13a26ee605663484c5f91a5854d5429518
Ivan Tham <itham@helios.local>
2013-05-22 02:03:53 -0700

    submitting a rating did not save, so missing template error was given. fixed it by alerting the user that the rating was not saved

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -7,9 +7,12 @@ class RatingsController < ApplicationController
     @rating = Rating.new(params[:rating])
     @rating.restaurant_id = @restaurant.id
     @rating.user_id = current_user.id
-    if @rating.save
-      respond_to do |format|
+    respond_to do |format|
+      if @rating.save
         format.html { redirect_to restaurant_path(@restaurant), :notice => "Your rating has been saved" }
+        format.js
+      else
+        format.html { redirect_to restaurant_path(@restaurant), :alert => "Your rating has not been saved" }
         format.js
       end
     end
@@ -18,9 +21,12 @@ class RatingsController < ApplicationController
   def update
     @restaurant = Restaurant.find_by_id(params[:restaurant_id])
     @rating = current_user.ratings.find_by_restaurant_id(@restaurant.id)
-    if @rating.update_attributes(params[:rating])
-      respond_to do |format|
+    respond_to do |format|
+      if @rating.update_attributes(params[:rating])
         format.html { redirect_to restaurant_path(@restaurant), :notice => "Your rating has been updated" }
+        format.js
+      else
+        format.html { redirect_to restaurant_path(@restaurant), :alert => "Your rating has not been updated" }
         format.js
       end
     end
