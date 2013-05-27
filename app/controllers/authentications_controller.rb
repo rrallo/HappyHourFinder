2a505d2228d0931eab6dbc6118555fde6150eb05
Ivan Tham <itham@helios.local>
2013-05-27 16:06:34 -0700

    Revert "logging in with another account does not work on heroku but does locally... fix attempt"
    This reverts commit 34dfd85f07356cb2edd302dd2e6a9d6590a6ce29.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,9 +3,9 @@ class AuthenticationsController < ApplicationController
     auth = request.env['omniauth.auth']
 
     # Try to find authentications first
-    authentication = Authentication.find_by_uid(auth['uid'])
+    authentication = Authentication.find_by_provider_and_uid(auth['provider'], auth['uid'])
 
-    if authentication != nil and authentication.user != nil
+    if authentication
       # Authentication found, sign the user in.
       flash[:notice] = 'Signed in successfully.'
 
