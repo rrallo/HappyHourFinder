f89cc6a6252174463087ed90172a41a617372f42
Ivan Tham <itham@helios.local>
2013-05-27 16:06:31 -0700

    Revert "attempt 2.2"
    This reverts commit bfeb7d17625a207422dee428f771b4a0a14c0634.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -5,7 +5,16 @@ class AuthenticationsController < ApplicationController
     # Try to find authentications first
     authentication = Authentication.find_by_uid(auth['uid'])
 
-    if authentication.nil?
+    if authentication
+      # Authentication found, sign the user in.
+      flash[:notice] = 'Signed in successfully.'
+
+      # Update information.
+      user = User.find(authentication.user_id)
+      user.update_fields(auth)
+
+      sign_in_and_redirect(:user, user)
+    else
       # Authentication not found, thus a new user.
       user = User.new
       user.update_fields(auth)
@@ -16,15 +25,6 @@ class AuthenticationsController < ApplicationController
         flash[:error] = 'Error while creating a user account. Please try again.'
         redirect_to root_url
       end
-    else
-      # Authentication found, sign the user in.
-      flash[:notice] = 'Signed in successfully.'
-
-      # Update information.
-      user = User.find(authentication.user_id)
-      user.update_fields(auth)
-
-      sign_in_and_redirect(:user, user)
     end
   end
 end
