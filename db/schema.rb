c4ef91feed812278f4a5f920465fd96069a3b604
Ivan Tham <itham@helios.local>
2013-06-03 22:47:04 -0700

    based on heroku logs, postgres is trying to store too big int into uid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130529183530) do
+ActiveRecord::Schema.define(:version => 20130604054507) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -46,17 +46,17 @@ ActiveRecord::Schema.define(:version => 20130529183530) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "location"
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",                  :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                       :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "uid"
+    t.integer  "uid",                    :limit => 8
     t.string   "first_name"
     t.string   "last_name"
     t.string   "link"
