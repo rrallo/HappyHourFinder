33bb8551e1913ce52b9b37391eecd6c79519f872
Lester Villaspir <lester.villaspir@gmail.com>
2013-06-04 20:27:44 -0700

    Added timestamps to reviews

---
+++ db/migrate/20130605023947_add_timestamps_to_reviews.rb
@@ -0,0 +1,7 @@
+class AddTimestampsToReviews < ActiveRecord::Migration
+  def change
+    change_table :reviews do |t|
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130604193715) do
+ActiveRecord::Schema.define(:version => 20130605023947) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -39,9 +39,11 @@ ActiveRecord::Schema.define(:version => 20130604193715) do
   end
 
   create_table "reviews", :force => true do |t|
-    t.string  "review"
-    t.integer "user_id"
-    t.integer "restaurant_id"
+    t.string   "review"
+    t.integer  "user_id"
+    t.integer  "restaurant_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
